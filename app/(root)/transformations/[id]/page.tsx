a8fcc8f21d80ac9ecc8fe5e9345288ebb68c2029
Koh Peter <peter.koh@cybo.co.jp>
2024-05-29 17:46:05 +0900

    added new folder structure for next

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TransformationsPage = ({ params }: { params: { id: String } }) => {
+    return (
+        <div>TransformationsPage</div>
+    )
+}
+
+export default TransformationsPage
+
+// not on localhost:3000/transformations
+// --> each will have its own id, using dynamic routes. use [] instead of ()
