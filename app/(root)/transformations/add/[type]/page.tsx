a9a475eda4ff21ec12800303b6bf162379d9ddce
peter@cybo.co.jp <peter@cybo.co.jp>
2024-06-04 14:23:35 +0900

    moved add folder to the right spot

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+import Header from '@/components/shared/header'
+
+
+const AddTransformationTypePage = () => {
+
+  return (
+    <Header
+      title="Transformation Title"
+      subtitle="Tranformation sub Title"
+    />
+  )
+}
+
+export default AddTransformationTypePage
